369e09ec7a861b0c15ea07843712f205601f523d
Dagon <wasilij18bus@gmail.com>
2024-01-20 18:20:34 +0300

    Я понялБ кроме функции

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,24 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Card 
+  foto="https://cdni.fancaps.net/file/fancaps-animeimages/13785769.jpg" 
+  msg="Это картинка терминатора!!!!" />
+ 
+  <Card 
+  foto="https://w.forfun.com/fetch/55/55b3a38aa5064e0a9061f9a9198d4e6f.jpeg"
+  msg="jbhcvbwubwiuvbibv" />
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Card from './components/Card.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Card
   }
 }
 </script>
---
+++ src/components/Card.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="card">
+    <div class="card" style="width: 18rem;">
+      <img v-if="openImg" v-bind:src="foto" class="card-img-top" alt="...">
+      <p class="card-text">{{msg}}</p>
+      <p class="card-text">{{foto}}</p>
+      <p class="card-text">{{disable}}</p>
+  </div>
+  <button v-on:click="remove" class="btn btn-primary">{{msgBtn}}</button>
+
+</div>
+</template>
+
+<script>
+/* eslint-disable */
+export default {
+  name: 'Card',
+  props: {
+    msg: String,
+    foto: String,
+    view: String
+  },
+  data() {
+    return{
+      openImg: true,
+      msgBtn: "Ликвидировать"
+    }
+  },  
+
+  methods:{
+    remove(){
+    this.openImg = !this.openImg;
+    if(this.openImg){
+      this.msgBtn = "Ликвидировать"
+    }else{
+      this.msgBtn = "Восстановить"
+    }
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
